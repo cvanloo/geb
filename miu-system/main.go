6cf44082613e2f6cfbc5de682e35f0100fc93473
Prince of Devils <104875-attaboy@users.noreply.framagit.org>
2023-02-13 09:03:52 +0100

    Well, uhmm...
    2023/02/13 09:01:51 start: `MI', end: `MU', max: 100000000
    2023/02/13 09:01:51 Permutations: 2
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 3
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 5
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 10
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 25
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 94
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 521
    Alloc = 0 MiB   TotalAlloc = 0 MiB      Sys = 8 MiB     NumGC = 0
    2023/02/13 09:01:51 Permutations: 4113
    Alloc = 3 MiB   TotalAlloc = 3 MiB      Sys = 12 MiB    NumGC = 0
    2023/02/13 09:01:51 Permutations: 46261
    Alloc = 14 MiB  TotalAlloc = 47 MiB     Sys = 30 MiB    NumGC = 10
    2023/02/13 09:01:53 Permutations: 745230
    Alloc = 332 MiB TotalAlloc = 1232 MiB   Sys = 396 MiB   NumGC = 29
    Killed

--- miu-system/main.go
+++ miu-system/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"strings"
+	"fmt"
+	"flag"
+	"log"
+	"runtime"
 )
 
 const (
@@ -12,46 +16,150 @@ const (
 	StringUU  = "UU"
 )
 
-func main() {
+type StepDesc string
 
+var (
+	StepStart   StepDesc = "start"
+	StepRuleI   StepDesc = "ruleI"
+	StepRuleII  StepDesc = "ruleII"
+	StepRuleIII StepDesc = "ruleIII"
+	StepRuleIV  StepDesc = "ruleIV"
+)
+
+type StepString struct {
+	Result string
+	Steps  []StepDesc
+}
+
+var startString = flag.String("s", "MI", "The initial string") 
+var endString = flag.String("e", "MU", "The target string")
+var maxDepth = flag.Int("d", 1000, "Maximal number of steps to apply to a string")
+
+func main() {
+	flag.Parse()
+	if len(*startString) < 2 {
+		panic("initial string must have a length of at least two characters")
+	}
+	log.Printf("start: `%s', end: `%s', max: %d", *startString, *endString, *maxDepth)
+	res := Solve(*startString, *endString, *maxDepth)
+	fmt.Printf("TOOK %d STEPS\n", len(res.Steps))
+	for _, s := range res.Steps {
+		fmt.Printf("%s -> ", s)
+	}
+	fmt.Println("RESULT FOUND!")
+}
+
+func Solve(startString, endString string, maxDepth int) StepString {
+	coll := []StepString{
+		{startString, []StepDesc{StepStart}},
+	}
+	for {
+		ncoll := []StepString{}
+		for _, s := range coll {
+			r1 := CreateDesc(RuleI(s.Result), s.Steps, StepRuleI)
+			r2 := CreateDesc(RuleII(s.Result), s.Steps, StepRuleII)
+			r3 := CreateDesc(RuleIII(s.Result), s.Steps, StepRuleIII)
+			r4 := CreateDesc(RuleIV(s.Result), s.Steps, StepRuleIV)
+			for _, r := range r1 {
+				if len(r.Steps) < maxDepth {
+					ncoll = append(ncoll, r)
+				}
+			}
+			for _, r := range r2 {
+				if len(r.Steps) < maxDepth {
+					ncoll = append(ncoll, r)
+				}
+			}
+			for _, r := range r3 {
+				if len(r.Steps) < maxDepth {
+					ncoll = append(ncoll, r)
+				}
+			}
+			for _, r := range r4 {
+				if len(r.Steps) < maxDepth {
+					ncoll = append(ncoll, r)
+				}
+			}
+			//ncoll = append(ncoll, r1...)
+			//ncoll = append(ncoll, r2...)
+			//ncoll = append(ncoll, r3...)
+			//ncoll = append(ncoll, r4...)
+		}
+		for _, c := range ncoll {
+			if c.Result == endString {
+				return c
+			}
+		}
+		if len(ncoll) == 0 {
+			log.Printf("No solution")
+			var nop StepString
+			return nop
+		}
+		coll = ncoll
+		log.Printf("Permutations: %d", len(coll))
+		PrintMemUsage()
+	}
+}
+
+func PrintMemUsage() {
+	var m runtime.MemStats
+	runtime.ReadMemStats(&m)
+	fmt.Printf("Alloc = %v MiB", bToMb(m.Alloc))
+	fmt.Printf("\tTotalAlloc = %v MiB", bToMb(m.TotalAlloc))
+	fmt.Printf("\tSys = %v MiB", bToMb(m.Sys))
+	fmt.Printf("\tNumGC = %v\n", m.NumGC)
+}
+
+func bToMb(b uint64) uint64 {
+    return b / 1024 / 1024
+}
+
+func CreateDesc(ss []string, prevSteps []StepDesc, currStep StepDesc) []StepString {
+	ncoll := []StepString{}
+	for _, s := range ss {
+		numPrevSteps := len(prevSteps)
+		nsteps := make([]StepDesc, numPrevSteps+1)
+		for i, li := range prevSteps {
+			nsteps[i] = li
+		}
+		nsteps[numPrevSteps] = currStep
+		ncoll = append(ncoll, StepString{s, nsteps})
+	}
+	return ncoll
 }
 
 // RuleI if the string ends in an 'I', a 'U' can be appended.
-func RuleI(s string) string {
+func RuleI(s string) []string {
 	last := s[len(s)-1]
 	if last == SymI {
-		return s + string(SymU)
+		return []string{s + string(SymU)}
 	}
-	return ""
+	return []string{}
 }
 
 // RuleII the string Mx can be turned into Mxx.
-func RuleII(s string) string {
+func RuleII(s string) []string {
 	x := s[1:]
-	xx := x + x
-	mxx := string(s[0]) + xx
-	return mxx
+	mxx := string(s[0]) + x + x
+	return []string{mxx}
 }
 
 // RuleIII one occurrence of III can be replaced by U.
 func RuleIII(s string) []string {
 	parts := strings.Split(s, StringIII)
 	strs := []string{}
-	for i := 0; i < len(parts); i++ {
-		ns := parts[0]
-		other := parts[1:]
-
-		if i < len(other) {
-			for j, p := range other {
-				if i == j {
-					ns = ns + string(SymU) + p
-				} else {
-					ns = ns + p
-				}
+	start := parts[0]
+	rest := parts[1:]
+	for i := 0; i < len(rest); i++ {
+		ns := start
+		for j, p := range rest {
+			if i == j {
+				ns = ns + string(SymU) + p
+			} else {
+				ns = ns + StringIII +  p
 			}
-			strs = append(strs, ns)
 		}
-
+		strs = append(strs, ns)
 	}
 	return strs
 }
@@ -60,18 +168,18 @@ func RuleIII(s string) []string {
 func RuleIV(s string) []string {
 	parts := strings.Split(s, StringUU)
 	strs := []string{}
-	for i := 0; i < len(parts); i++ {
-		ns := parts[0]
-		for j, p := range parts[1:] {
+	start := parts[0]
+	rest := parts[1:]
+	for i := 0; i < len(rest); i++ {
+		ns := start
+		for j, p := range rest {
 			if j == i {
 				ns = ns + p
 			} else {
 				ns = ns + StringUU + p
 			}
 		}
-		if ns != s {
-			strs = append(strs, ns)
-		}
+		strs = append(strs, ns)
 	}
 	return strs
 }
--- miu-system/main_test.go
+++ miu-system/main_test.go
@@ -2,6 +2,60 @@ package main
 
 import "testing"
 
+func TestRuleI(t *testing.T) {
+	tests := []struct {
+		test    string
+		results map[string]bool
+	}{
+		{test: "MI", results: map[string]bool{"MIU": false}},
+		{test: "MIII", results: map[string]bool{"MIIIU": false}},
+		{test: "MIUUUIIU", results: map[string]bool{}},
+	}
+
+	for caseNum, testCase := range tests {
+		res := RuleI(testCase.test)
+		for _, r := range res {
+			if _, ok := testCase.results[r]; !ok {
+				t.Errorf("%d: invalid result: `%s'", caseNum, r)
+			} else {
+				testCase.results[r] = true
+			}
+		}
+		for k, v := range testCase.results {
+			if !v {
+				t.Errorf("%d: missing result: `%s'", caseNum, k)
+			}
+		}
+	}
+}
+
+func TestRuleII(t *testing.T) {
+	tests := []struct {
+		test    string
+		results map[string]bool
+	}{
+		{test: "MI", results: map[string]bool{"MII": false}},
+		{test: "MIII", results: map[string]bool{"MIIIIII": false}},
+        {test: "MIUUUIIU", results: map[string]bool{"MIUUUIIUIUUUIIU": false}},
+	}
+
+	for caseNum, testCase := range tests {
+		res := RuleII(testCase.test)
+		for _, r := range res {
+			if _, ok := testCase.results[r]; !ok {
+				t.Errorf("%d: invalid result: `%s'", caseNum, r)
+			} else {
+				testCase.results[r] = true
+			}
+		}
+		for k, v := range testCase.results {
+			if !v {
+				t.Errorf("%d: missing result: `%s'", caseNum, k)
+			}
+		}
+	}
+}
+
 func TestRuleIII(t *testing.T) {
 	tests := []struct {
 		test    string
@@ -57,6 +111,26 @@ func TestRuleIV(t *testing.T) {
 			if !v {
 				t.Errorf("%d: missing result: `%s'", caseNum, k)
 			}
+		}
+	}
+}
+
+func TestSolve(t *testing.T) {
+	start := "MI"
+	end := "MIU"
+	expect := StepString{
+		"MIU",
+		[]StepDesc{StepStart, StepRuleI},
+	}
+	res := Solve(start, end, 1000)
+
+	if res.Result != expect.Result {
+		t.Errorf("want: %s, got invalid result: `%s'", expect.Result, res.Result)
+	}
+	for i, step := range expect.Steps {
+		actualStep := res.Steps[i]
+		if actualStep != step {
+			t.Errorf("want: %s, got invalid step: `%s'", step, actualStep)
 		}
 	}
 }
